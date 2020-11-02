81939c3a552f9cec874790f2e2c20b240567880e
Florent Chretien <florent.chretien@outlook.com>
2020-11-02 23:28:58 +0000

    add raspi_ruediger

---
+++ raspi_ruediger/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/40_Git/ruediger/raspi_ruediger/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/kinetic/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/ubuntu/40_Git/ruediger/raspi_ruediger/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/40_Git/ruediger/raspi_ruediger/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ubuntu/40_Git/ruediger/raspi_ruediger/src:$ROS_PACKAGE_PATH"
