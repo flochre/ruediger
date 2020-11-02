aa5e997c5caa17800e4837a3fd2a9323aa1794fe
Florent Chretien <florent.chretien@outlook.com>
2020-11-02 23:36:56 +0000

    deleted build/ and devel/ from git

--- raspi_ruediger/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/40_Git/ruediger/raspi_ruediger/src
-# Build directory: /home/ubuntu/40_Git/ruediger/raspi_ruediger/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
