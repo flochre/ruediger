dc0a4ad1d56383329d66ed7bc8b3e6561b2bad6f
Florent Chretien <flopubs@gmail.com>
2020-11-22 21:07:34 +0100

    clean code

--- arduino_ruediger/include/drive.hpp
+++ arduino_ruediger/include/drive.hpp
@@ -8,22 +8,13 @@
 
 class Drive {
 
-    // Creating handlers of Node, IMU message, quaternion and ROS publisher.
-    ros::NodeHandle *nh_;
-    // ros::Subscriber<std_msgs::Int32> *drive_vx;
-    // ros::Subscriber<std_msgs::Int32> *drive_vy;
-    // ros::Subscriber<std_msgs::Int32> *drive_vteta;
-
-    ros::Subscriber<geometry_msgs::Twist> *cmd_vel;
+  // Creating handlers of Node, IMU message, quaternion and ROS publisher.
+  ros::NodeHandle *nh_;
+  ros::Subscriber<geometry_msgs::Twist> *cmd_vel;
 
   public:
     Drive(void);
 
-    // void vx_msg(const std_msgs::Int32& msg);
-    // void vy_msg(const std_msgs::Int32& msg);
-    // void vteta_msg(const std_msgs::Int32& msg);
-
-    // void setup(ros::NodeHandle *nh, char sub_vx_name[], char sub_vy_name[], char sub_vteta_name[], ros::Subscriber<std_msgs::Int32>::CallbackT cb_vx, ros::Subscriber<std_msgs::Int32>::CallbackT cb_vy, ros::Subscriber<std_msgs::Int32>::CallbackT cb_vteta);
     void setup(ros::NodeHandle *nh, char sub_cmd_vel[], ros::Subscriber<geometry_msgs::Twist>::CallbackT cb_cmd_vel);
     void loop(void);
 };
--- arduino_ruediger/src/drive.cpp
+++ arduino_ruediger/src/drive.cpp
@@ -2,16 +2,8 @@
 
 Drive::Drive(void){}
 
-// void Drive::setup(ros::NodeHandle *nh, char sub_vx_name[], char sub_vy_name[], char sub_vteta_name[], 
 void Drive::setup(ros::NodeHandle *nh, char sub_cmd_vel[], ros::Subscriber<geometry_msgs::Twist>::CallbackT cb_cmd_vel) {
     nh_ = nh;
-  
-    // drive_vx = new ros::Subscriber<std_msgs::Int32>(sub_vx_name, cb_vx);
-    // nh_->subscribe(*drive_vx);
-    // drive_vy = new ros::Subscriber<std_msgs::Int32>(sub_vy_name, cb_vy);
-    // nh_->subscribe(*drive_vy);
-    // drive_vteta = new ros::Subscriber<std_msgs::Int32>(sub_vteta_name, cb_vteta);
-    // nh_->subscribe(*drive_vteta);
 
     cmd_vel = new ros::Subscriber<geometry_msgs::Twist>(sub_cmd_vel, cb_cmd_vel);
     nh_->subscribe(*cmd_vel);
