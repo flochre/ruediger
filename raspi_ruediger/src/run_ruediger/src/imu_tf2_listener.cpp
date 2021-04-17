f959e04e7e355130cf29aeb35a713582a34e2624
Florent Chretien <florent.chretien@outlook.com>
2021-04-17 11:25:13 +0000

    add run_ruediger.lauch + transform on the raspi

---
+++ raspi_ruediger/src/run_ruediger/src/imu_tf2_listener.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "imu_listener");
+
+  ros::NodeHandle node;
+
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    geometry_msgs::TransformStamped transformStamped;
+    try{
+      transformStamped = tfBuffer.lookupTransform("base_link", "imu_data",
+                               ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+  }
+
+  return 0;
+};
